96ffa444914f935e71e42e67be1b2903b1a29ebd
davidjcuadros <cuadrosdavid01@gmail.com>
2025-11-17 20:13:02 -0500

    apiGateway Listo

---
+++ apigatewayproy/src/main/java/apigateway/apigatewayproy/ApigatewayproyApplication.java
@@ -0,0 +1,13 @@
+package apigateway.apigatewayproy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayproyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayproyApplication.class, args);
+	}
+
+}
