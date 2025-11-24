74fd40c8174c51b81fea3d176a51a7c9e42d5e50
Fer03Liz <fjlizarazog@gmail.com>
2025-11-24 18:54:33 -0500

    Y existe un solo micropagos

--- logica/MicroPagos/Model/EstadoPago.cs
+++
@@ -1,7 +0,0 @@
-namespace MicroPagos.Model;
-
-public class EstadoPago
-{
-    public int Id { get; set; }
-    public string Estado { get; set; }
-}
--- logica/MicroPagos/Model/MetodoPago.cs
+++
@@ -1,7 +0,0 @@
-namespace MicroPagos.Model;
-
-public class MetodoPago
-{
-    public int Id { get; set; }
-    public string Tipo { get; set; }
-}
